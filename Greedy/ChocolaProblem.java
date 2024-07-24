3b84271aa0a53e2a893167042014b9c1d567a8cb
JayPant <jaypant1111@gmail.com>
2024-07-25 01:43:31 +0530

    add job sequencing problem

---
+++ Greedy/ChocolaProblem.java
@@ -0,0 +1,10 @@
+package Greedy;
+
+public class ChocolaProblem {
+    public static void main(String[] args) {
+        int n =4 , m =6;
+        Integer cV[] = {2,1,3,1,4};
+        Integer cH[] = {2,1,3,1,4};
+
+    }
+}
